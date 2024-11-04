9e47454ce83d550a04eda7da71407d6091d32c96
Wawwior <45405580+Wawwior@users.noreply.github.com>
2024-11-04 19:05:37 +0100

    Is this anything?

--- common/src/main/java/me/wawwior/temple/Temple.java
+++ common/src/main/java/me/wawwior/temple/Temple.java
@@ -4,11 +4,6 @@ import me.wawwior.temple.registry.Registrant;
 import me.wawwior.temple.registry.RegistryHelper;
 import me.wawwior.temple.registry.blocks.TempleBlocks;
 import me.wawwior.temple.registry.items.TempleItems;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Rarity;
-import net.minecraft.world.level.block.Block;
 
 public final class Temple {
 
